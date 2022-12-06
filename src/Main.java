5cc175720c69df4a3c25d993fc132d291f6dd5b8
07elbekdev <masrapovelbek24@gmail.com>
2022-12-06 13:18:01 +0600

    Everything is clear

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-    static String[]words = {"envelope","cantelope","something","You're","the best","thing","ever","So","what","I","you","all" +
-            "creative","soughts","all","that","one","Ronlado","player"};
+    static String[] words = {"something", "You're", "the best", "thing", "ever", "So", "what", "I", "you", "all" +
+            "creative", "soughts", "all", "that", "one", "Ronlado", "player", ",", "think", "all", "is"};
+
     public static void main(String[] args) throws InterruptedException {
         System.out.println("3");
         TimeUnit.SECONDS.sleep(1);
@@ -18,22 +19,22 @@ public class Main {
 
         Random rand = new Random();
         for (int i = 0; i < 10; i++) {
-        System.out.print(words[rand.nextInt(9)]+" ");
+            System.out.print(words[rand.nextInt(9)] + " ");
         }
         System.out.println();
 
-       double start = LocalTime.now().toNanoOfDay();
+        double start = LocalTime.now().toNanoOfDay();
 
         Scanner scan = new Scanner(System.in);
         String typeWords = scan.nextLine();
 
         double end = LocalTime.now().toNanoOfDay();
-        double elapsedTime = end-start;
-        double seconds = elapsedTime/1000000000.0;
+        double elapsedTime = end - start;
+        double seconds = elapsedTime / 1000000000.0;
         System.out.println(seconds);
         int numChars = typeWords.length();
-        int WPM = (int) (((numChars/5)/seconds)*60);
+        int WPM = (int) (((numChars / 5) / seconds) * 60);
 
-        System.out.println("Your speed is : "+WPM+" WPM");
+        System.out.println("Your speed is : " + WPM + " WPM");
     }
 }
