69bb494ce38b706af641339e4a022c35f25e44fb
07elbekdev <masrapovelbek24@gmail.com>
2022-12-06 15:33:15 +0600

    brown is adding

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.concurrent.TimeUnit;
 public class Main {
     static final String GREEN = "\u001B[32m";
     static final String BLUE = "\u001B[34m";
-    static final String BROWN = "";
+    static final String BROWN = "\u001B[31m";
     static String[] words = {"something", "You're", "the best", "thing", "ever", "So", "what", "I", "you",
             "creative", "sought", "all", "that", "one", "Ronaldo", "player", ",", "think", "all", "is"};
 
