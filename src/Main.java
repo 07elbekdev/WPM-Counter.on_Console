1ef72455cf330f1aedc44fea772698a6923520cd
07elbekdev <masrapovelbek24@gmail.com>
2022-12-07 13:18:34 +0600

    everything is clear

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,14 @@ import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
+    static final String GREEN = "\u001B[32m";
     static final String BLUE = "\u001B[34m";
-    static final String BROWN = "\u001B[31m";
     static final String WHITE = "\u001B[37m";
     static final String YELLOW = "\u001B[33m";
     static final String CYAN = "\u001B[36m";
-    static String[] words = {"something", "You're", "the best", "thing", "ever", "So", "what", "I", "you",
-            "creative", "sought", "all", "that", "one", "player", "think", "all", "is"};
-    public static void main(String[] args) throws InterruptedException {
+    static String[] words = {"In","1920","people","12", "something", "You're", "the best", "thing", "ever", "So", "what", "I", "you", "creative", "sought", "all", "that", "one", "player", "think", "all", "is"};
 
+    public static void main(String[] args) throws InterruptedException {
 
         Random rand = new Random();
         for (int i = 0; i < 10; i++) {
@@ -20,10 +19,10 @@ public class Main {
         }
 
         System.out.println();
-        System.out.println(BLUE+"5"+BLUE);
+        System.out.println(BLUE + "5" + BLUE);
         TimeUnit.SECONDS.sleep(1);
 
-        System.out.println(BROWN+"4"+BROWN);
+        System.out.println(GREEN + "4" + GREEN);
         TimeUnit.SECONDS.sleep(1);
 
         System.err.println("3");
@@ -45,7 +44,7 @@ public class Main {
         double end = LocalTime.now().toNanoOfDay();
         double elapsedTime = end - start;
         double seconds = elapsedTime / 1000000000.0;
-        System.out.println(WHITE+seconds+WHITE);
+        System.out.println(WHITE + seconds + WHITE);
         int numChars = typeWords.length();
         int WPM = (int) (((numChars / 5) / seconds) * 60);
 
