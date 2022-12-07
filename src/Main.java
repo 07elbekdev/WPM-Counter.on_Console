a06db4c5d2b6136d1a552bf18a993f2b0d1de0e0
07elbekdev <masrapovelbek24@gmail.com>
2022-12-07 12:50:40 +0600

    something is ready.

--- src/Main.java
+++ src/Main.java
@@ -28,7 +28,7 @@ public class Main {
         for (int i = 0; i < 10; i++) {
             System.out.print(YELLOW + words[rand.nextInt(9)] + " ");
         }
-//        System.out.println();
+        System.out.println();
 
         double start = LocalTime.now().toNanoOfDay();
 
