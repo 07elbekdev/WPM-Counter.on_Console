63305db1d416485bd480a0c2f9d1e70894c80e22
07elbekdev <masrapovelbek24@gmail.com>
2022-12-18 20:27:01 +0600

    Everything is up to-date

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ import java.util.concurrent.TimeUnit;
 public class Main {
     static final String GREEN = "\u001B[32m";
     static final String BLUE = "\u001B[34m";
-
     static final String WHITE = "\u001B[37m";
     static final String YELLOW = "\u001B[33m";
     static final String CYAN = "\u001B[36m";
